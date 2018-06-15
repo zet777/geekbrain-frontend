b7b28576e8d2401c4ea40432f047e4d67836d5fd
rzet777 <rzet777@gmail.com>
2018-06-15 19:35:03 +0300

    upd

---
+++ task1.js
@@ -0,0 +1,12 @@
+/* Task 1 */
+function cut(){
+	var str = process.argv[2];
+	if(undefined !==str && str.length && 1<=str.length && str.length<=10000){
+		process.stdout.write(str.replace(/[Р,р,К,к,Н,н]/g, ""));	
+	}
+	else{
+		process.stdout.write("Строка должна быть не пустой и не более 10000");
+	}	
+} 
+cut();
+/* Put your code here */
